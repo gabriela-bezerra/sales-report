ab36d1d1c9233ceeb3a1d6d14d4b25db96eae4f9
Gabriela <gabrielaa.bdasilva@gmail.com>
2022-08-19 21:26:37 -0700

    Function with a dictionary created

--- sales_report.py
+++ sales_report.py
@@ -1,30 +1,30 @@
 """Generate sales report showing total melons each salesperson sold."""
 
 
-salespeople = []  # empty list
-melons_sold = []  # empty list
+# salespeople = []  # empty list
+# melons_sold = []  # empty list
 
-f = open('sales-report.txt')  # opneing the file
+# f = open('sales-report.txt')  # opneing the file
 
-for line in f:  # iterating over the file
-    line = line.rstrip()  # removing white spaces
-    entries = line.split('|')  # spliting the list with |
-    salesperson = entries[0]  # assigning firs item to salesperson
-    # assigning third item to melons and converting it to a integer
-    melons = int(entries[2])
+# for line in f:  # iterating over the file
+#     line = line.rstrip()  # removing white spaces
+#     entries = line.split('|')  # spliting the list with |
+#     salesperson = entries[0]  # assigning firs item to salesperson
+#     # assigning third item to melons and converting it to a integer
+#     melons = int(entries[2])
 
-    if salesperson in salespeople:  # statment to compare info
-        # adding salesperson to the empty list
-        position = salespeople.index(salesperson)
-        melons_sold[position] += melons  # counting the melons
+#     if salesperson in salespeople:  # statment to compare info
+#         # adding salesperson to the empty list
+#         position = salespeople.index(salesperson)
+#         melons_sold[position] += melons  # counting the melons
 
-    else:
-        salespeople.append(salesperson)  # a ppending salesperson
-        melons_sold.append(melons)  # a ppending melons
+#     else:
+#         salespeople.append(salesperson)  # a ppending salesperson
+#         melons_sold.append(melons)  # a ppending melons
 
 
-for i in range(len(salespeople)):  # printing the file
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+# for i in range(len(salespeople)):  # printing the file
+#     print(f'{salespeople[i]} sold {melons_sold[i]} melons')
 
 """
 Improvements:
@@ -33,3 +33,35 @@ Improvements:
     - Pass the file as an argument
 
 """
+
+
+def get_melons_sales(filename):
+
+    sales_report_data = open(filename)
+
+    sales = {}
+
+    for line in sales_report_data:
+        line = line.rstrip()
+        salesperson, total_sale, melons_sold = line.split("|")
+
+        sales[salesperson] = melons_sold
+
+        """ Tried adding total value and total amount 
+        
+        to the dictinonary as a list
+        """
+
+        # for salesperson, reports in sales.item():
+        #     print(salesperson).upper()
+
+        #     for total, melons in reports.item():
+        #         print(f'{total} - {melons} ')
+
+        for person, melons in sales.items():
+            print(f'{person} sold the total of {melons} melons')
+
+    sales_report_data.close
+
+
+get_melons_sales("sales-report.txt")
